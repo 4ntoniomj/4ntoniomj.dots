10c90efac4d553cd4e69159efc2e56536fedd197
4ntoniomj <mj.antonio.madrid@gmail.com>
2026-09-22 22:31:20 +0200

    fix(wezterm): ocultar por completo la barra superior de pestanas y sesiones

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -103,8 +103,10 @@ config.window_decorations = "RESIZE"
 
 -- Barra nativa deshabilitada: Zellij gestiona las pestañas verticales
 config.enable_tab_bar = false
-config.use_fancy_tab_bar = true
-config.hide_tab_bar_if_only_one_tab = false
+config.show_tabs_in_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = false
 
 config.window_frame = {
